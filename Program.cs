fda67ddfcfa3545545363a53c3d00062f257b2c9
Roman Korchnev <92043977+r0m04k0@users.noreply.github.com>
2023-02-10 15:36:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная #1 - Инкапсуляция");
+            Console.WriteLine("Выполнил - Корчнев Роман");
+            Console.WriteLine("\nЗадание 1: ");
+
+            Complex a = new Complex() { Real = 3, Imag = 0.5 };
+            Complex b = new Complex() { Real = 2, Imag = 0.8 };
+
+            a.Add(b);
+            b.Substract(a);
+            a.Multiply(a);
+            a.Divide(b);
+
+            Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
+            Console.WriteLine("b = {0} + {1}i", b.Real, b.Imag);
+
+            Console.WriteLine("\nЗадание 2: ");
+
+            Student student1 = new Student("Коля", "male", "808000", 22, 180.0f, 70.5f);
+            Student student2 = new Student("Аня", "female", "1a1a00", 22, 158.0f, 60f);
+
+            string newStudent = student1.Hybreed(student1, student2);
+            Console.WriteLine(newStudent);
+
+
+
+        }
+    }
+}
+
+
---
+++ Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Student
+    {
+        public
+            string Name { get; set; }
+            string Gender { get; set; }
+            string HairColor { get; set; }
+            int Age { get; set; }
+            float Weight { get; set; }
+            float Height { get; set; }
+
+        public Student(string name, string gender, string hairColor, int age, float weight, float height)
+        {
+            Name = name;
+            Gender = gender;
+            HairColor = hairColor;
+            Age = age;
+            Weight = weight;
+            Height = height;
+        }
+
+        public string Hybreed(Student student1, Student student2)
+        {
+            string NewName = "";
+            for (int i = 0; i < student2.Name.Length; i += 1)
+            {
+                NewName += student2.Name.Substring(i, 1) + student1.Name.Substring(i, 1);
+            }
+
+            string NewGender;
+                
+            if (student1.Gender == "male" && student2.Gender == "male") { NewGender = "male"; }
+            else if (student1.Gender == "female" && student2.Gender == "female") { NewGender = "female"; }
+            else { NewGender = "transgender"; }
+            
+            string NewColor = "";
+            for (int i = 0; i < student2.HairColor.Length; i += 2)
+            {
+                NewColor += student2.HairColor.Substring(i, 1) + student1.HairColor.Substring(i, 1);
+            }
+
+            int NewAge = (student1.Age + student2.Age)/2;
+
+            float NewWeight = student1.Weight + student2.Weight;
+
+            float NewHeight = student1.Height + student2.Height;
+
+            return (NewName + "\n" + NewGender + "\n" + NewColor + "\n" + NewAge + "\n" + NewWeight + "\n" + NewHeight);
+                
+        }
+    }
+
+    
+
+}
